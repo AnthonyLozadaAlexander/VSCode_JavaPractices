ef8070a6b78d73bbae3defa3e475f8bd3cad3ed6
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-10 23:29:44 -0500

    update commit

--- demo/src/main/java/com/example_prueba1/for.java
+++
@@ -1,5 +0,0 @@
-package com.example_prueba1;
-
-public class for {
-  
-}
---
+++ demo/src/main/java/com/example_prueba1/for_1.java
@@ -0,0 +1,8 @@
+package com.example_prueba1;
+
+public class for_1 {
+  public static void main(String[] args) {
+      
+  }
+  
+}
