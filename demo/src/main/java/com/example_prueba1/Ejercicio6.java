58eed0c334f1a16f795803110026423e785a1dcd
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-10 21:33:35 -0500

    update commit

---
+++ demo/src/main/java/com/example_prueba1/Ejercicio6.java
@@ -0,0 +1,29 @@
+package com.example_prueba1;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+  public static void main(String[] args){
+
+    //Bucle while
+    Scanner input = new Scanner (System.in);
+    int contador;
+
+    System.out.println("El numero debe ser menor a 15 para cumplir la condicion del bucle");
+    contador = input.nextInt();
+
+    System.out.println("--------------------------------------------------");
+
+    while(contador <= 15){
+
+      System.out.println("El contador es: " + contador);
+      contador++; // Incremento de la variable contador para evitar un bucle infinito
+
+    }
+
+    
+    System.out.println("--------------------------------------------------");
+
+  }
+  
+}
