61ad6a8e4d7a356fe60e63c89f63cb997499725b
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-11 00:05:07 -0500

    update commit

--- demo/src/main/java/com/example_prueba1/for_1.java
+++ demo/src/main/java/com/example_prueba1/for_1.java
@@ -3,6 +3,7 @@ package com.example_prueba1;
 public class for_1 {
   public static void main(String[] args) {
 
+    /* 
     for(int contador = 0; contador < 10; contador++){
       System.out.println("Estoy en la iteracion N° " + contador + " del contador hasta llegar 10");
     }
@@ -17,17 +18,18 @@ public class for_1 {
     }
 
     System.out.println();
-
-    int suma = 0;
     System.out.println("--------------------------------------------------\n");
+    */
+
+    int suma;
 
     for(int contador = 0; contador < 10; contador++){
       // Contador + 1 es para que contador empieze desde 1 y no desde 0
-      System.out.println("Estoy en la iteracion N° " + (contador + 1) + " del contador hasta llegar 10");
+      System.out.println("Estoy en la iteracion N° " + contador + " del contador hasta llegar 10");
 
       suma = 5 + contador;
 
-      if(suma > 7){
+      if(suma >= 7){
         contador = 11;
       }
 
