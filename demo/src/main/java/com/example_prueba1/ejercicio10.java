1d613528d2644d1f0819106406a5b1ac8049b792
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-11 19:29:57 -0500

    Agregar clase para calcular el valor absoluto de un número ingresado por el usuario

---
+++ demo/src/main/java/com/example_prueba1/ejercicio10.java
@@ -0,0 +1,18 @@
+package com.example_prueba1;
+import java.util.Scanner;
+
+
+public class ejercicio10 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int a, absolute;
+
+    System.out.println("Ingrese un numero: ");
+    a = input.nextInt();
+
+    absolute = Math.abs(a);
+
+    System.out.println("El valor absoluto de " + a + " es: " + absolute);
+      
+  } 
+}
