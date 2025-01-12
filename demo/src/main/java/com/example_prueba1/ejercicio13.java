ac2c19a8de816b2ca654155294d0f08d453014da
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-11 23:30:51 -0500

    Agregar clase para imprimir números del 1 al 30 y permitir al usuario establecer un límite

---
+++ demo/src/main/java/com/example_prueba1/ejercicio13.java
@@ -0,0 +1,28 @@
+package com.example_prueba1;
+
+import java.util.Scanner;
+
+public class ejercicio13 {
+  public static void main(String[] args){
+     System.out.println("Imprimiendo numeros del 1 al 30");
+
+     for(int i = 1; i <= 30; i++){
+      System.out.println(i);
+     }
+
+      System.out.println("-----------------------------------------");
+      Scanner input = new Scanner(System.in);
+      int limit;
+
+      System.out.println("Imprimir numeros del limite que el usuario desee\n");
+      System.out.println("Ingrese el limite: ");
+      limit = input.nextInt();
+
+      for(int i = 1; i <= limit; i++){
+        System.out.println(i);
+      }
+     
+
+  }
+  
+}
