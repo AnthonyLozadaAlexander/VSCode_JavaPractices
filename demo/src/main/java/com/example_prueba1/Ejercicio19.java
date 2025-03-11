49816025c50e11312d13c9027f5621aa712ca078
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-10 23:12:30 -0500

    Add Ejercicio19 and Ejercicio20 classes for user input and array processing

---
+++ demo/src/main/java/com/example_prueba1/Ejercicio19.java
@@ -0,0 +1,36 @@
+package com.example_prueba1;
+
+import java.util.Scanner;
+
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] nums = new int[10];
+        System.out.println("-----------------------------------");
+        System.out.println("Ingrese 10 numeros en el array");
+        System.out.println("-----------------------------------");
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.print("Ingrese un numero en la posicion["+i+"]: ");
+            nums[i] = input.nextInt();
+        }
+
+        System.out.println("-----------------------------------");
+        System.out.println("Elementos del array: ");
+        for(int i = 0; i < nums.length; i++){
+            System.out.println("Nums["+i+"]: "+ nums[i]);
+        }
+
+        int max = nums[0];
+        int Indice = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+                Indice = i;
+            }
+        }
+        System.out.println("-----------------------------------");
+        System.out.println("El numero mayor del array es nums["+Indice+"] = " + max);
+    }
+}
---
+++ demo/src/main/java/com/example_prueba1/Ejercicio20.java
@@ -0,0 +1,7 @@
+package com.example_prueba1;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+
+    }
+}
