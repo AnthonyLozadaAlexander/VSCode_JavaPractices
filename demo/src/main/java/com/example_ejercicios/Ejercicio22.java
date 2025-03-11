e1c932a7b186ef1ac7e9e539fd1db50344d466c6
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-11 17:57:57 -0500

    Enhance Ejercicio22 to display the index and value of the maximum number from user input

--- demo/src/main/java/com/example_ejercicios/Ejercicio22.java
+++ demo/src/main/java/com/example_ejercicios/Ejercicio22.java
@@ -12,15 +12,22 @@ public class Ejercicio22 {
             System.out.print("Nums["+i+"] = ");
             nums[i] = input.nextInt();
         }
+        System.out.println("---------------------------------------");
 
         int mayor = nums[0];
+        int indice = 0;
         System.out.println("Los numeros ingresados son: ");
         for(int i = 0; i < nums.length; i++){
             System.out.println("Nums["+i+"] = "+ nums[i]);
             if(nums[i] > mayor){
                 mayor = nums[i];
+                indice = i;
             }
         }
+
+        System.out.println("-----------------------------------------");
+        System.out.println("El numero mayor es: ");
+        System.out.println("num["+indice+"] = " + mayor);
 
     }
 }
