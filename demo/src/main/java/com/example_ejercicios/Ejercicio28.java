84f02b3534266f00dc71cb165aad00d4477fa6fc
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-18 23:10:19 -0500

    Add Ejercicio28 for processing product sales and calculating total revenue

---
+++ demo/src/main/java/com/example_ejercicios/Ejercicio28.java
@@ -0,0 +1,66 @@
+package com.example_ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio28 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String codigo = "";
+        int cantKilos = 0;
+        float precio = 0;
+        float total = 0;
+        float facturacionTotal=0;
+        int count = 0;
+        int totalKilos = 0;
+
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println("\nFactura N°("+i+")");
+            System.out.println("Ingrese el codigo del producto");
+            System.out.println("-> ");
+            codigo = input.nextLine();
+            do {
+                System.out.println("\nIngrese la cantidad de kilos");
+                System.out.print("-> ");
+                if(!input.hasNextInt()){
+                    System.out.println("Error: Ingrese un número entero positivo");
+                    input.next(); // Limpiar el buffer
+                    continue;
+                }
+                cantKilos = input.nextInt();
+                if (cantKilos <= 0) {
+                    System.out.println("Error: La cantidad de kilos debe ser mayor a 0");
+                }
+            }while(cantKilos <= 0);
+
+            do {
+                System.out.println("\nIngrese el precio");
+                System.out.print("-> ");
+                if(!input.hasNextFloat()){
+                    System.out.println("Error: Ingrese un número decimal positivo");
+                    input.next(); // Limpiar el buffer
+                    continue;
+                }
+                precio = input.nextFloat();
+
+                if(precio <= 0){
+                    System.out.println("Error: El precio debe ser mayor a 0");
+                }
+            }while(precio <= 0);
+
+            total = cantKilos * precio;
+            facturacionTotal = facturacionTotal + total;
+
+            if(total > 1000){
+                count = count+ 1;
+            }
+            totalKilos = totalKilos + cantKilos;
+
+        }
+
+        System.out.println("\nDetalles De Las Ventas");
+        System.out.println("Facturacion Total: " + facturacionTotal);
+        System.out.println("Total De Kilos Vendidos: " + totalKilos);
+    }
+}
