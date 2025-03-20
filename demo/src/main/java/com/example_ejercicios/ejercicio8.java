55c371a62b53d04ea1a488846091a8001a9d8897
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-19 21:30:38 -0500

    Improve error handling and output formatting in ejercicio8

--- demo/src/main/java/com/example_ejercicios/ejercicio8.java
+++ demo/src/main/java/com/example_ejercicios/ejercicio8.java
@@ -6,25 +6,23 @@ import java.util.Scanner;
 public class ejercicio8 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int num, num2;
+    int num;
 
     try{ //?Try catch para evitar errores de tipo de dato
     System.out.println("Digite el primer numero: ");
     num = input.nextInt();
 
-    System.out.println("");
-
     if(num > 0){
-      System.out.println("El numero "+ num + " es positivo");
+      System.out.println("\nEl numero "+ num + " es positivo");
     }
     else if(num < 0){
-      System.out.println("El numero "+ num + " es negativo");
+      System.out.println("\nEl numero "+ num + " es negativo");
     }
     else{
-      System.out.println("El numero " + num + " es cero");
+      System.out.println("\nEl numero " + num + " es cero");
     }
-    }catch (InputMismatchException e){
-      System.out.println("Error: Dato no valido");
+    }catch (InputMismatchException e){ // si se ingresa texto en vez de numeros se activara el catch
+      System.out.println("Error404: Dato no valido");
     }
   }   
 }
