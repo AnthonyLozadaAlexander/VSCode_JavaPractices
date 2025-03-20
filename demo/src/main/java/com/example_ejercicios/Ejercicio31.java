ad78060e29946045af0e4b06391deb7c468b6b94
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-19 22:16:48 -0500

    Enhance Ejercicio31 to improve date input validation and error handling

--- demo/src/main/java/com/example_ejercicios/Ejercicio31.java
+++ demo/src/main/java/com/example_ejercicios/Ejercicio31.java
@@ -16,21 +16,57 @@ public class Ejercicio31 {
                 input.next();
                 continue;
             }
+            dia = input.nextInt();
             if(dia <= 0){
                 System.out.println("\nError: Dia Invalido\n");
             }
             else if((dia >=1 && dia <= 31) || (dia >= 1 && dia <= 30) || (dia >= 1 && dia <= 28)){
-                System.out.println("\nDia Ingresado: "+ dia);
+                System.out.println("\nDia Ingresado: "+ dia+"\n");
                 do{
+                    input = new Scanner(System.in);
                     System.out.println("Ingrese el mes");
                     System.out.print("-> ");
-                }while(mes <= 0);
+                    if(!input.hasNextInt()){
+                        System.out.println("\nError: Dato Invalido\n");
+                        input.next();
+                        continue;
+                    }
+                    mes = input.nextInt();
+                    if(mes <= 0){
+                        System.out.println("\nError: Mes Invalido\n");
+                    }
+                    else if(mes >= 1 && mes <= 12){
+                        System.out.println("\nMes Ingresado: "+ mes+"\n");
+                        do {
+                            input = new Scanner(System.in);
+                            System.out.println("Ingrese el anio");
+                            System.out.print("-> ");
+                            if (!input.hasNextInt()) {
+                                System.out.println("\nError: Dato Invalido\n");
+                                input.next();
+                                continue;
+                            }
+                            anio = input.nextInt();
+
+                            if(anio == 0){
+                                System.out.println("\nError: Anio Invalido\n");
+                            }
+                            else if(anio != 0){
+                                System.out.println("Año Ingresado: "+anio);
+                                System.out.println("\nFecha: "+dia+"/"+mes+"/"+anio);
+                                System.out.println("La Fecha Es Correcta");
+                            }
+                        }while(anio == 0);
+                    }
+                    else{
+                        System.out.println("\nError: Mes Invalido\n");
+                    }
+                }while(mes <= 0 || mes > 12);
             }
             else{
                 System.out.println("Fecha Invalida: Dia: "+dia+" Incorrecto");
             }
-            dia = input.nextInt();
-        }while(dia <=0);
+        }while(dia <=0 || dia > 31);
 
     }
 }
