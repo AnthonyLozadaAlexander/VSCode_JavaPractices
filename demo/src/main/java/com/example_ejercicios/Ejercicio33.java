a3d8a051a58b221683b5bd10d57dce5705d53a86
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-21 00:48:17 -0500

    Enhance Ejercicio33 to improve input validation for sold liters and price, ensuring positive values are entered

--- demo/src/main/java/com/example_ejercicios/Ejercicio33.java
+++ demo/src/main/java/com/example_ejercicios/Ejercicio33.java
@@ -6,8 +6,8 @@ public class Ejercicio33 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String [] codigo = new String[5];
-        float cantLitros=0, precioLitro=0, facturacionTotal=0, count=0, countLitros=0, montoTotal =0;
-
+        float cantLitros=0, precioLitro=0, facturacionTotal=0, count=0, countLitros=0, montoTotal =0, datoLitros=0;
+        boolean On = false;
         for(int i = 0; i < 5; i++){
             input = new Scanner(System.in);
             facturacionTotal = 0;
@@ -24,7 +24,6 @@ public class Ejercicio33 {
                 }
                 else{
                     System.out.println("\nCodigo Ingresado: " + codigo[i] + "\n");
-
                     do{
                         System.out.println("Ingrese La Cantidad Vendida Por Litro");
                         System.out.print("-> ");
@@ -32,11 +31,42 @@ public class Ejercicio33 {
                         if(!input.hasNextFloat()){
                             System.out.println("\nError: Debe Ingresar Datos Numericos\n");
                             input.next();
-                            continue;
+
+                            if(i == 0 || i < 5){
+                                do{
+                                    System.out.println("Ingrese La Cantidad Vendida Por Litro");
+                                    System.out.print("-> ");
+                                    if(!input.hasNextFloat()) {
+                                        System.out.println("\nError: Debe Ingresar Datos Numericos\n");
+                                        input.next();
+                                        On = false;
+                                    }
+                                    else{
+                                        float temp = Float.parseFloat(input.next());
+                                        if (temp > 0) {
+                                            datoLitros = temp;
+                                        On = true;
+                                        } else
+                                        {
+                                            System.out.println("\nError: Debe ingresar un valor mayor a 0\n");
+                                            On = false;
+                                        }
+                                    }
+
+                                }while(On == false);
+                            }
+                            else{
+                                continue;
+                            }
                         }
-                        cantLitros = input.nextFloat();
+                        if(datoLitros > 0){
+                            cantLitros = datoLitros;
+                        }
+                        else{
+                            cantLitros = input.nextFloat();
+                        }
                         if(cantLitros <= 0){
-                            System.out.println("\nError: Debe Ingresar La Cantidad Mayor a Cero");
+                            System.out.println("\nError: Debe Ingresar La Cantidad Mayor a Cero\n");
                         }
                         else{
                             countLitros = countLitros + cantLitros;
@@ -47,12 +77,15 @@ public class Ejercicio33 {
 
                                 if(!input.hasNextFloat()){
                                     System.out.println("\nError: Debe Ingresar Datos Numericos\n");
+                                    input.next();
+                                    continue;
                                 }
                                 precioLitro = input.nextFloat();
                                 if(precioLitro <= 0){
                                     System.out.println("\nError: Debe Ingresar El Precio Mayor a Cero\n");
                                 }
                                 else{
+
                                     System.out.println("\nPrecio Ingresado: " + precioLitro + "\n");
                                 }
                             }while(precioLitro <= 0);
@@ -68,6 +101,7 @@ public class Ejercicio33 {
             }
 
         }
+
         System.out.println("\n-----------------------------");
         System.out.println("           FACTURA");
         System.out.println("\n-----------------------------");
