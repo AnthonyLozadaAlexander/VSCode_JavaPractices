b0f22d47cc8d579dd6789f7915d301dd3ceff52e
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-25 00:12:25 -0500

    Refactor ejercicio12 to improve academic status messages for clarity and consistency

--- demo/src/main/java/com/example_ejercicios/ejercicio12.java
+++ demo/src/main/java/com/example_ejercicios/ejercicio12.java
@@ -24,46 +24,48 @@ public class ejercicio12 {
 
               switch (x) {
                   case 10:
-                      System.out.println("Estado Academico: Calificacion De Honor");
+                      System.out.println("Estado Academico: Excelente");
                       break;
 
                   case 9:
-                      System.out.println("Estado Academico: Sobresaliente");
+                      System.out.println("Estado Academico: Muy bueno");
                       break;
 
                   case 7:
-                      System.out.println("Estado Academico: Notable");
+                      System.out.println("Estado Academico: Bueno");
                       break;
 
                   case 6:
-                      System.out.println("Estado Academico: Notable");
+                      System.out.println("Estado Academico: Bueno");
                       break;
 
                   case 5:
-                      System.out.println("Estado Academico: Aprobado");
+                      System.out.println("Estado Academico: Suficiente");
                       break;
 
                   case 4:
-                      System.out.println("Supletorio");
+                      System.out.println("Estado Academico: Necesita mejorar");
                       break;
 
                   case 3:
-                      System.out.println("Supletorio");
+                      System.out.println("Estado Academico: Recuperacion");
                       break;
 
                   case 2:
-                      System.out.println("Supletorio");
+                      System.out.println("Estado Academico: Recuperacion");
                       break;
+
                   case 1:
-                      System.out.println("Reprobado");
+                      System.out.println("Estado Academico: Recuperacion");
                       break;
 
                   case 0:
-                      System.out.println("Reprobado");
+                      System.out.println("Estado Academico: Reprobado");
                       break;
 
                   default:
                       System.out.println("Calificacion no valida");
+                      break;
 
               }
           }
