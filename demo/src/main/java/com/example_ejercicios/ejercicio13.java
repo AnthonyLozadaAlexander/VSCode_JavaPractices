bd0d719f1907b49344ec955bdac839904e91bb87
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-25 00:12:04 -0500

    Refactor ejercicio13 for improved readability and formatting consistency

--- demo/src/main/java/com/example_ejercicios/ejercicio13.java
+++ demo/src/main/java/com/example_ejercicios/ejercicio13.java
@@ -3,26 +3,25 @@ package com.example_ejercicios;
 import java.util.Scanner;
 
 public class ejercicio13 {
-  public static void main(String[] args){
-     System.out.println("Imprimiendo numeros del 1 al 30");
+    public static void main(String[] args) {
+        System.out.println("Imprimiendo numeros del 1 al 30");
 
-     for(int i = 1; i <= 30; i++){
-      System.out.println(i);
-     }
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i);
+        }
 
-      System.out.println("-----------------------------------------");
-      Scanner input = new Scanner(System.in);
-      int limit;
+        Scanner input = new Scanner(System.in);
+        System.out.println("-----------------------------------------");
+        int limit;
 
-      System.out.println("Imprimir numeros del limite que el usuario desee\n");
-      System.out.println("Ingrese el limite: ");
-      limit = input.nextInt();
+        System.out.println("\nImprimir numeros del limite que el usuario desee\n");
+        System.out.println("Ingrese el limite: ");
+        limit = input.nextInt();
 
-      for(int i = 1; i <= limit; i++){
-        System.out.println(i);
-      }
-     
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i);
+        }
 
-  }
-  
+    }
+
 }
