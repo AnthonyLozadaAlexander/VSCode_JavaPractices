f1472711d0e01422b1560dfea6f3476c06610181
zDefcon20 <antholozadarequelme@gmail.com>
2025-05-04 03:25:48 -0500

    Add Ejercicio34 class with initial structure for quadratic equation handling

---
+++ demo/src/main/java/com/example_ejercicios/Ejercicio34.java
@@ -0,0 +1,14 @@
+package com.example_ejercicios;
+import java.util.Scanner;
+public class Ejercicio34 {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    public Cuadratica(){
+
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
