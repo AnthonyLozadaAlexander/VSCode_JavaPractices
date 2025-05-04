784cc708d41b2ab759a855469e7b720b2d848d0a
zDefcon20 <antholozadarequelme@gmail.com>
2025-05-04 03:30:20 -0500

    Add Cuadratica class to handle quadratic equation input and determine real solutions

--- demo/src/main/java/com/example_ejercicios/Ejercicio34.java
+++ demo/src/main/java/com/example_ejercicios/Ejercicio34.java
@@ -1,10 +1,19 @@
 package com.example_ejercicios;
-import java.util.Scanner;
+import javax.swing.*;
 public class Ejercicio34 {
     int a = 0;
     int b = 0;
     int c = 0;
     public Cuadratica(){
+        a = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor de a: "));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor de b: "));
+        c = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor de c: "));
+        double determinante = Math.pow(b,2) - (4*a*c);
+        if(determinante > 0){
+
+        }else{
+            System.out.println("\nEl Determinante es negativo, no tiene solución real");
+        }
 
 
     }
