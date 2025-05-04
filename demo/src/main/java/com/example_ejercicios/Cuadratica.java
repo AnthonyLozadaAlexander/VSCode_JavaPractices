1cffcb6ffa3814d2f0aae9a83b3cfc11eabf792a
zDefcon20 <antholozadarequelme@gmail.com>
2025-05-04 03:35:29 -0500

    Rename Ejercicio34 to Cuadratica and update class structure for quadratic equation handling

--- demo/src/main/java/com/example_ejercicios/Cuadratica.java
+++ demo/src/main/java/com/example_ejercicios/Cuadratica.java
@@ -1,6 +1,6 @@
 package com.example_ejercicios;
 import javax.swing.*;
-public class Ejercicio34 {
+public class Cuadratica {
     int a = 0;
     int b = 0;
     int c = 0;
@@ -10,17 +10,18 @@ public class Ejercicio34 {
         c = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor de c: "));
         double determinante = Math.pow(b,2) - (4*a*c);
         if(determinante > 0){
+            // se aplican las dos soluciones a la formula general
             double x1 = ((b * (-1)) + Math.sqrt(determinante)) / (2 * a);
             double x2 = ((b * (-1)) - Math.sqrt(determinante)) / (2 * a);
+            System.out.println("El valor de x1 = " + x1);
+            System.out.println("El valor de x2 = " + x2);
         }
         else
         {
             System.out.println("\nEl Determinante es negativo, no tiene solución real");
         }
-
-
     }
     public static void main(String[] args) {
-
+        new Cuadratica();
     }
 }
