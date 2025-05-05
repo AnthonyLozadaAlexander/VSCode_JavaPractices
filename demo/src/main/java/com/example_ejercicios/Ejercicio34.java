8f1a8ee3d59c666be46b204c60fc5af54f6337ee
zDefcon20 <antholozadarequelme@gmail.com>
2025-05-04 22:31:43 -0500

    Add Ejercicio34 class with basic structure for user input

---
+++ demo/src/main/java/com/example_ejercicios/Ejercicio34.java
@@ -0,0 +1,13 @@
+package com.example_ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------------------------");
+        System.out.println("Fecha De Nacimiento (dd/mm/yyyy)");
+        System.out.println("--------------------------------");
+        String fecha = input.nextLine();
+    }
+}
